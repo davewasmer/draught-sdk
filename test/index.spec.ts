e525379d9824ac7e08f8f0aee1862951f40e8f82
Dave Wasmer <davewasmer@gmail.com>
2022-06-22 23:08:12 -0600

    fix tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,2 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-function
-describe.skip('too hard to test for now', () => {});
+describe('too hard to test for now', () => {
+  it('should test', () => {
+    return;
+  });
+});
